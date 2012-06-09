65202151c4160040b73c9c3b9b65851211e6fd7e
billy bob <sebortiz@gmail.com>
2012-06-09 04:09:21 -0700

    fixed import statements and changed position of plt.show() so that the rolling_mean gets displayed when simply using python from command line. Previously it only worked in ipython.

--- example.py
+++ example.py
@@ -1,29 +1,38 @@
 from withings import * 
-import numpy as np
 import matplotlib.pyplot as plt #for plotting weights
+from datetime import datetime
+from pandas import *
 #-----------------------------------
 #get your consumer key and secret after registering 
 #as a developer here: https://oauth.withings.com/en/partner/add
 consumer_key='your_consumer_key_here_1111111111111111111111111111111111111111'
 consumer_secret='your_consumer_secret_here_1111111111111111111111111111111111111111111'
+
 #the first argument here is the email address you used to register
 #for a Withings developer account
 wobj=Withings('your_registered_email_address@example.com',consumer_key,consumer_secret)
 (dates,weights)=wobj.get_weights()
 print "You have ",len(weights)," weight measurements recorded"
-#pandas allows to build a timeseries object which will display
+
+#pandas allows us to build a timeseries object which will display
 #dates along the x-axis when plotted and can have rolling window 
 #functions applied to it, like rolling_mean,rolling_median,rolling_std,etc.
 rdates=[datetime.fromtimestamp(i) for i in dates]
 ts=Series(weights,index=rdates)
+
 #the marker='o' option will put a dot at each measurement point
 #which, if you mouse-over will display the value
 ts.plot(marker='o',color='b')
 plt.ylabel('Weight(lbs)')
-plt.show()
+
 #this will overlay a smoothed curve which suggests the trend
 #of the underlying data
 rolling_mean(ts,len(weights)/float(7)).plot(color='r',linewidth='3')
+plt.show()
+
+
+
+
 #if you don't have pandas, or have problems installing it,
 #the code below makes a basic plot without the nice date labels
 #on the x-axis
