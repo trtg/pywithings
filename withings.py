a4ba6be800b0bd838905330fcf250b5f5c456c06
Sebastian Ortiz <sebortiz@gmail.com>
2012-09-07 22:38:52 -0700

    now have support for all the different measurement types and properly return independent date arrays for every measurement type if requested

--- withings.py
+++ withings.py
@@ -1,16 +1,22 @@
 import hashlib#for computing hash
 from rauth.service import OAuth1Service #see https://github.com/litl/rauth for more info
 import shelve #for persistent caching of tokens, hashes,etc.
+import json
+import time
+
 
 #get your consumer key and secret after registering as a developer here: https://oauth.withings.com/en/partner/add
 
 #FIXME add method to set default units and make it an optional argument to the constructor
 class Withings:
-    def __init__(self,email_address,consumer_key,consumer_secret,cache_name='tokens.dat'):
+    def __init__(self,email_address,consumer_key,consumer_secret,debug=False,cache_name='tokens.dat'):
         if email_address == None:
             raise Exception("You must specify the email address associated with your Withings account")
         #cache stores tokens and hashes on disk so we avoid
         #requesting them every time.
+        self.KG_TO_POUNDS=2.20462262
+        self.CM_TO_IN=1/2.54
+        self.debug=debug
         self.cache=shelve.open(cache_name,writeback=False)
         self.email_address = email_address
         self.password_hash = self.cache.get('withings_password_hash')
@@ -42,7 +48,8 @@ class Withings:
             self.get_request_token()
             got_access_token=self.get_access_token()
             if( not got_access_token):
-                print "Error: Unable to get access token"
+                pass
+                #print "Error: Unable to get access token"
 
         #Maybe you always need to recompute the hash/get a new once to avoid expiration?
         #self.reqhash = self.cache.get('withings_reqhash',None)
@@ -69,12 +76,12 @@ class Withings:
         authorize_url=self.oauth.get_authorize_url(self.request_token)
         #the pin you want here is the string that appears after oauth_verifier on the page served
         #by the authorize_url
-        print 'Visit this URL in your browser then login: ' + authorize_url
+        print("Visit this URL in your browser then login: " + authorize_url)
         self.pin = raw_input('Enter PIN from browser: ')
         self.cache['withings_request_token']=self.request_token
         self.cache['withings_request_token_secret']=self.request_token_secret
         self.cache['withings_pin']=self.pin
-        print "withings_pin is ",self.cache.get('withings_pin')
+        print("withings_pin is "+self.cache.get('withings_pin'))
 
     def need_request_token(self):
         #created this method because i'm not clear when request tokens need to be obtained, or how often
@@ -89,13 +96,13 @@ class Withings:
                 request_token_secret=self.request_token_secret,
                 params={'oauth_verifier':self.pin})
         data=response.content
-        print response.content
+        print(response.content)
         self.access_token=data.get('oauth_token',None)
         self.access_token_secret=data.get('oauth_token_secret',None)
         self.cache['withings_access_token']=self.access_token
         self.cache['withings_access_token_secret']=self.access_token_secret
         if not(self.access_token) or not(self.access_token_secret):
-            print "access token expired "
+            print("access token expired ")
             return False
         else:
             return True
@@ -103,7 +110,7 @@ class Withings:
     def get_public_key_and_user_id(self):
         if self.reqhash==None:
             self.compute_hash()
-        print "using hash ",self.reqhash
+        print("using hash ",self.reqhash)
         params={'action': 'getuserslist','email':self.email_address,'hash':self.reqhash} #how to determine public key and userid
         response=self.oauth.get(
                 'http://wbsapi.withings.net/account',
@@ -114,7 +121,7 @@ class Withings:
                 )
 
         data=response.content
-        print data
+        print(data)
         body=data['body']
         #FIXME- allow handling multiple users
         self.public_key=body['users'][0]['publickey']
@@ -123,9 +130,9 @@ class Withings:
         self.cache['withings_public_key']=self.public_key
         self.is_public = int(body['users'][0]['ispublic'])
         self.cache['withings_is_public'] = self.is_public
-        print "get_public_key response is ",response
-        print "get_public_key data is ",data
-        print "get_public_key body is ",body
+        print("get_public_key response is ",response)
+        print("get_public_key data is ",data)
+        print("get_public_key body is ",body)
         #FIXME refactor this to return values which you then store where needed
         #return public_key,user_id,is_public
         
@@ -143,16 +150,17 @@ class Withings:
         data=response.content
         body=data['body']
         once=body['once']
-        print "once is ",once
+        print("once is ",once)
         reqhash = ":".join([self.email_address,self.password_hash,once])
         self.reqhash=hashlib.md5(reqhash).hexdigest()
         #save the hash for subsequent uses. this should only need to be done once,
         #unless you change your password?
         self.cache['withings_reqhash']=self.reqhash
 
-    #only need to do this once
+    #this may need to be run every time the user registers a new device type (scale, BP cuff)
+    #ispublic parameter is a bitmask, 1 = scale, 4 = BP cuff
     def make_data_public(self):
-        params={'action': 'update','userid': self.user_id,'publickey': self.public_key,'ispublic': '7'} #set all data to be public
+        params={'action': 'update','userid': self.user_id,'publickey': self.public_key,'ispublic': '5'} #set all data to be public
         response=self.oauth.get(
                 'http://wbsapi.withings.net/user',
                 params=params,
@@ -160,11 +168,38 @@ class Withings:
                 access_token_secret=self.access_token_secret,
                 header_auth=True)
 
-    def get_weights(self):
+    #this method returns the raw json for the get_meas call,
+    #other convenience methods within this wrapper in turn extract specific measurement types from that json
+    #if specified, startdate and enddate must be in '%Y-%m-%d' format
+    def get_meas(self,devtype=0,startdate=None,enddate=None):
         if (self.public_key==None):
             self.get_public_key_and_user_id()
-        print "user_id in get_weights ",self.user_id
-        params={'action': 'getmeas','userid': self.user_id,'publickey': self.public_key,'startdate': '0'} #how to determine first and last name
+        print("user_id in get_meas ",self.user_id)
+        
+        if startdate is None and enddate is None :
+            params={'action': 'getmeas',
+                    'userid': self.user_id,
+                    'publickey': self.public_key,
+                    'startdate': '0'} 
+        elif startdate is None:
+            params={'action': 'getmeas',
+                    'userid': self.user_id,
+                    'publickey': self.public_key,
+                    'startdate': '0',
+                    'enddate': str(int(time.mktime(time.strptime(enddate,'%Y-%m-%d'))))} 
+        else:
+            params={'action': 'getmeas',
+                    'userid': self.user_id,
+                    'publickey': self.public_key,
+                    'startdate': str(int(time.mktime(time.strptime(startdate,'%Y-%m-%d'))))} 
+
+        #The devtype parameter is not really a bitmask, this is poorly documented. A value of 5 seems to retrieve blood pressure
+        #cuff and scale data, 1 gets just scale data, 4 gets just blood pressure data, but in order to get height data
+        #no devtype parameter at all should be sent. Contrary to what the documentation says, if you send devtype=0, you get nothing back.
+        #The wrapper will behave as though the documentation is correct and simply send no devtype if the user specifies 0 or does not 
+        #pass a devtype argument at all
+        if devtype in [1,4,5]:
+                params['devtype']=str(devtype)
 
         response=self.oauth.get(
                 'http://wbsapi.withings.net/measure',
@@ -174,19 +209,80 @@ class Withings:
                 header_auth=True)
 
         data=response.content
+        if self.debug:
+            fid=open('measurements.json','w')
+            json.dump(data,fid)
+            fid.close()
+        return data
+
+    def extract_meas(self,data,meastype=1):
         body=data['body']
         measuregroups=body['measuregrps']
-        print len(measuregroups)
-        weight_data=[]
+        print(len(measuregroups))
+        data=[]
         dates=[]
+        #there can be a more parameter indicating that more data remains
+        #to be retrieved
         for mgrp in measuregroups:
             measures=mgrp['measures']
-            #print len(measures)
+            print "number of measures ",len(measures)
             for measure in measures:
-                if measure['type']==1:
+                if measure['type']==meastype:
                    #use encoded exponent for unit and convert to pounds
-                   one_weight=(measure['value']*(10**measure['unit']))*2.20462262
-                   weight_data.append(one_weight)
+                   one_meas=(measure['value']*(10**measure['unit']))
+                   data.append(one_meas)
                    dates.append(mgrp['date'])
-        return (dates,weight_data)
+        return (dates,data) 
 
+    def get_weights(self,data,units="kg"):
+        if units == "lbs":
+            (dates,data)=self.extract_meas(data,meastype=1)
+            data=[i*self.KG_TO_POUNDS for i in data]
+        else:
+            #default to "kg"
+            (dates,data)=self.extract_meas(data,meastype=1)
+
+        return (dates,data)
+
+    def get_height(self,data,units="cm"):
+        if units == "in":
+            (dates,data)=self.extract_meas(data,meastype=4)
+            data=[i*self.CM_TO_IN for i in data]
+        else:
+            #default to cm
+            (dates,data)=self.extract_meas(data,meastype=4)
+        return (dates,data)
+
+    def get_fat_free_mass(self,data,units="kg"):
+        if units == "lbs":
+            (dates,data)=self.extract_meas(data,meastype=5)
+            data=[i*self.KG_TO_POUNDS for i in data]
+        else:
+            #default to kg
+            (dates,data)=self.extract_meas(data,meastype=5)
+        return (dates,data)
+
+    def get_fat_ratio(self,data):
+            (dates,data)=self.extract_meas(data,meastype=6)
+            return (dates,data)
+
+    def get_fat_mass_weight(self,data,units="kg"):
+        if units == "lbs":
+            (dates,data)=self.extract_meas(data,meastype=8)
+            data=[i*self.KG_TO_POUNDS for i in data]
+        else:
+            #default to kg
+            (dates,data)=self.extract_meas(data,meastype=8)
+        return (dates,data)
+
+    def get_diastolic_blood_pressure(self,data):
+        (dates,data)=self.extract_meas(data,meastype=9)
+        return (dates,data)
+
+    def get_systolic_blood_pressure(self,data):
+        (dates,data)=self.extract_meas(data,meastype=10)
+        return (dates,data)
+
+    def get_heart_pulse(self,data):
+        (dates,data)=self.extract_meas(data,meastype=11)
+        return (dates,data)
